ebdc24368eaa45163a728f5d3373d7fc987e1cf1
Israel Sampaio <israelsampaio28@gmail.com>
2022-06-25 10:25:05 -0400

    feat: creates header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,19 @@
-const Header: React.FC = () => <p>kk</p>;
+import Image from 'next/image';
+import Link from 'next/link';
 
-export { Header };
+import classNames from 'classnames/bind';
+import styles from './header.module.scss';
+
+const cx = classNames.bind(styles);
+
+export const Header: React.FC = () => (
+  <header className={cx('container', 'header')}>
+    <Link href="/">
+      <a>
+        <Image src="/images/Logo.svg" alt="logo" width={238} height={25} />
+      </a>
+    </Link>
+  </header>
+);
 
 export default Header;
